c3bbe6b8474f076b47532d2361689f1265140b19
jansenosorio <dev.osorio93@gmail.com>
2023-05-18 11:11:31 -0300

    FEAT: created hotels card - final version

--- src/components/CardHotel.js
+++ src/components/CardHotel.js
@@ -1,21 +1,76 @@
 import styled from 'styled-components';
 
-export default function CardHotel() {
+export default function CardHotel(props) {
+  const { image, name, acomodationType, vacancies } = props;
+
   return (
     <CardContainer>
-      <Card/>
+      <Card>
+        <img src={image} alt='hotel'/>
+        <h1>{name}</h1>
+        <div>
+          <h2>{'Tipos de acomodação:'}</h2>
+          <h3>{acomodationType}</h3>
+        </div>
+        <div>
+          <h2>{'Vagas disponíveis:'}</h2>
+          <h3>{vacancies}</h3>
+        </div>
+      </Card>
     </CardContainer>
   );
 };
 
 const CardContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
     width: 100%;
     height: auto;
 `;
 
 const Card = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
     width: 196px;
     height: 264px;
     background: #EBEBEB;
     border-radius: 10px;
+
+    box-sizing: border-box;
+    padding: 16px 14px;
+
+    img {
+      width: 168px;
+      height: 109px;
+      border-radius: 5px;
+    }
+
+    h1 {
+      font-family: 'Roboto';
+      font-style: normal;
+      font-weight: 400;
+      font-size: 20px;
+      line-height: 23px;
+      color: #343434;
+    }
+
+    h2 {
+      font-family: 'Roboto';
+      font-style: normal;
+      font-weight: 700;
+      font-size: 12px;
+      line-height: 14px;
+      color: #3C3C3C;
+      margin-bottom: 5px;
+    }
+
+    h3 {
+      font-family: 'Roboto';
+      font-style: normal;
+      font-weight: 400;
+      font-size: 12px;
+      line-height: 14px;
+      color: #3C3C3C;
+    }
 `;
--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -1,6 +1,7 @@
 import MainTitle from '../../../components/MainTitle';
 import MainSubtitle from '../../../components/Subtitle';
 import useGetHotels from '../../../hooks/api/useHotel';
+import CardHotel from '../../../components/CardHotel';
 
 export default function Hotel() {
   const hotels = useGetHotels();
@@ -13,6 +14,7 @@ export default function Hotel() {
       <MainSubtitle>
         {'Primeiro, escolha seu hotel'}
       </MainSubtitle>
+      <CardHotel image={'https://www.kayak.com.br/rimg/himg/44/fe/90/leonardo-61545-147068318-531165.jpg?width=1366&height=768&crop=true'} name={'Driven Hotel'} acomodationType={'single'} vacancies={'108'}/>
     </>
   );
 }
