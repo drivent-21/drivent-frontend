d9468fc87e53a4ffec05f0bae5a4d9f469ea00e6
jansenosorio <dev.osorio93@gmail.com>
2023-05-27 10:44:25 -0300

    FEAT: created final page at hotels

--- src/hooks/api/useHotel.js
+++ src/hooks/api/useHotel.js
@@ -3,10 +3,22 @@ import useToken from '../useToken';
 
 import * as hotelsApi from '../../services/hotelsApi';
 
-export default function useGetHotels() {
+function useGetHotels() {
   const token = useToken();
 
   const { data } = useAsync(() => hotelsApi.getHotels(token));
 
   return data;
 };
+
+function useRegisterBookings(body) {
+  const token = useToken();
+
+  const { data } = useAsync(() => hotelsApi.registerBooking(body, token)); 
+
+  return data;
+};
+
+const useHotels = { useGetHotels, useRegisterBookings };
+
+export default useHotels;
